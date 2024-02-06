e3ebec07e659befbca7c47e02330f3377889eccd
Arturo Clavero <artclave@z3t2c1.42bangkok.com>
2024-02-06 15:59:02 +0700

    exit handles negative exit codes

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,13 +6,11 @@
 /*   By: artclave <artclave@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 01:16:02 by artclave          #+#    #+#             */
-/*   Updated: 2024/02/06 08:28:53 by artclave         ###   ########.fr       */
+/*   Updated: 2024/02/06 15:57:41 by artclave         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "execution.h"
-
-static int	is_nflag(char *str);
 
 void	exec_echo(char **cmd_array)
 {
@@ -21,10 +19,9 @@ void	exec_echo(char **cmd_array)
 
 	i = 0;
 	n_flag = 0;
+	//GET ENV EXPANDABLES $
 	while (cmd_array[++i])
 	{
-		if (cmd_array[i][0] == '\0')
-			continue ;
 		if (is_nflag(cmd_array[i]) && n_flag + 1 == i)
 		{
 			n_flag++;
@@ -39,7 +36,7 @@ void	exec_echo(char **cmd_array)
 	exit (0);
 }
 
-static int	is_nflag(char *str)
+int	is_nflag(char *str)
 {
 	int	i;
 
