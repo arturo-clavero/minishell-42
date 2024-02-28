50a78eaa875bf730d56f89aa7db25701123d0e03
Ugolin Ollé <hello@ugolin-olle.com>
2024-02-28 11:09:33 +0700

    chore: unify some functions in a same file

--- srcs/utils/ft_exit.c
+++ srcs/utils/ft_exit.c
@@ -6,18 +6,18 @@
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 10:55:01 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2024/02/26 10:58:36 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2024/02/28 11:08:40 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
 /**
- * @brief Exits the program with the specified status after printing the given content to the standard error.
+ * @brief Exit with a message and a status
  *
- * @param content The content to be printed to the standard error before exiting. Can be NULL.
- * @param status The exit status of the program.
+ * @param char *content - the content to print
+ * @param int status - the exit status
+ * @return void
  */
 void	ft_exit(char *content, int status)
 {
