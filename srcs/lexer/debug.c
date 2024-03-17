b83267d4a03703bf935467357a20b130a296d7ce
Ugolin Ollé <hello@ugolin-olle.com>
2024-03-18 01:15:58 +0700

    style(norm): update for norminette

--- srcs/lexer/debug.c
+++ srcs/lexer/debug.c
@@ -6,7 +6,7 @@
 /*   By: ugolin-olle <ugolin-olle@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 21:45:52 by ugolin-olle       #+#    #+#             */
-/*   Updated: 2024/03/17 00:16:22 by ugolin-olle      ###   ########.fr       */
+/*   Updated: 2024/03/18 00:54:07 by ugolin-olle      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ void	ft_debug_lexer(t_lexer *lexer)
 {
 	while (lexer)
 	{
+		printf("\nLexer: \n");
 		printf("str: %s\n", lexer->str);
 		printf("token: %d\n", lexer->token);
+		printf("\n");
 		lexer = lexer->next;
 	}
 	ft_free_lexer(lexer);
